fa775d971deaa9df1f4e4ab580dee51bf09bf586
ryota-toyomura <miitoyo291@gmail.com>
2023-12-15 16:55:20 +0900

    住所のjsの記述変更

--- form/assets/js/form.js
+++ form/assets/js/form.js
@@ -1,4 +1,19 @@
 $(function () {
+    $('.ajaxzip3').on('click', function () {
+        AjaxZip3.zip2addr('zip', '', 'pref', 'addr1', 'addr2', 'addr3');
+        //成功時に実行する処理
+        AjaxZip3.onSuccess = function () {
+            $('.addr3').focus();
+        };
+        //失敗時に実行する処理
+        AjaxZip3.onFailure = function () {
+            alert('郵便番号に該当する住所が見つかりません');
+        };
+        return false;
+    });
+});
+
+$(function () {
     $('form').validate({
         rules: {
 
